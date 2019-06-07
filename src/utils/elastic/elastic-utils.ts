cb93bb168ed5eed5cc905bf5a8ba318174249f9f
Hannes Ingelhag <hannes.ingelhag@clasohlson.se>
2019-06-07 23:27:27 +0200

    Create util for elastic

---
+++ src/utils/elastic/elastic-utils.ts
@@ -0,0 +1,51 @@
+import { Client } from "@elastic/elasticsearch";
+const { ELASTIC_USERNAME, ELASTIC_PASSWORD, ELASTIC_CLOUD_ID } = process.env;
+
+interface ShardsResponse {
+  total: number;
+  successful: number;
+  failed: number;
+  skipped: number;
+}
+
+interface Explanation {
+  value: number;
+  description: string;
+  details: Explanation[];
+}
+
+export interface SearchResponse<T> {
+  took: number;
+  timed_out: boolean;
+  _scroll_id?: string;
+  _shards: ShardsResponse;
+  hits: {
+    total: {
+      value: number;
+    };
+    max_score: number;
+    hits: Array<{
+      _index: string;
+      _type: string;
+      _id: string;
+      _score: number;
+      _source: T;
+      _version?: number;
+      _explanation?: Explanation;
+      fields?: any;
+      highlight?: any;
+      inner_hits?: any;
+      matched_queries?: string[];
+      sort?: string[];
+    }>;
+  };
+  aggregations?: any;
+}
+
+export const elasticClient = new Client({
+  cloud: {
+    id: `${ELASTIC_CLOUD_ID}`,
+    username: `${ELASTIC_USERNAME}`,
+    password: `${ELASTIC_PASSWORD}`
+  }
+});
