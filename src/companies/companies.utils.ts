c82d99e665374eb13a4760981d662313d21c9bb3
Hannes Ingelhag <hannes.ingelhag@clasohlson.se>
2019-06-09 16:58:51 +0200

    Move function to utils

--- src/companies/companies.utils.ts
+++ src/companies/companies.utils.ts
@@ -1,7 +1,44 @@
-import { CompanyOrderByInput } from "./companies.typedefs";
+import {
+  CompanyOrderByInput,
+  Aggregations,
+  ElasticAggregations
+} from "./companies.typedefs";
 
+/**
+ * Return an array with length of two, first with attributes and then if it should be asc/desc
+ * @param sort the parameter
+ */
 export const getSortByParameterByEnum = (
   sort: CompanyOrderByInput
 ): string[] => {
   return sort.toString().split("_");
 };
+
+/**
+ * Map the aggregations from Elastic model.
+ * @param aggregations
+ */
+export const mapAggregations = (
+  aggregations: ElasticAggregations
+): Aggregations => ({
+  maxAssets: aggregations.agg_max_assets.value,
+  minAssets: aggregations.agg_min_assets.value,
+  maxMarketValue: aggregations.agg_max_marketValue.value,
+  minMarketValue: aggregations.agg_min_marketValue.value,
+  maxProfits: aggregations.agg_max_profits.value,
+  minProfits: aggregations.agg_min_profits.value,
+  maxSales: aggregations.agg_max_sales.value,
+  minSales: aggregations.agg_min_sales.value,
+  countries: aggregations.agg_terms_country.buckets.map(country => ({
+    count: country.doc_count,
+    key: country.key
+  })),
+  industries: aggregations.agg_terms_industry.buckets.map(industry => ({
+    count: industry.doc_count,
+    key: industry.key
+  })),
+  sectors: aggregations.agg_terms_sector.buckets.map(sector => ({
+    count: sector.doc_count,
+    key: sector.key
+  }))
+});
